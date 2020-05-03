c47b534a51768a122429b76e3382e950a43d3456
Dmitry Shamrilo <shamrilods@gmail.com>
2020-05-04 00:21:36 +0300

    Добавляет класс на загрузку боди

--- source/js/script.js
+++ source/js/script.js
@@ -8,6 +8,7 @@ import result from './modules/result.js';
 import form from './modules/form.js';
 import social from './modules/social.js';
 import FullPageScroll from './modules/full-page-scroll';
+import {initPage} from './modules/init-page.js';
 
 // init modules
 mobileHeight();
@@ -18,6 +19,7 @@ chat();
 result();
 form();
 social();
+initPage();
 
 const fullPageScroll = new FullPageScroll();
 fullPageScroll.init();
