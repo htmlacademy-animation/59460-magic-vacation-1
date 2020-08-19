9330db38f7ba12077e9a77f793af058c810d9500
Dmitry Shamrilo <shamrilods@gmail.com>
2020-08-19 09:00:16 +0300

    Правит ошибки eslint

--- source/js/modules/rules.js
+++ source/js/modules/rules.js
@@ -1,9 +1,9 @@
 export default () => {
   window.addEventListener(`load`, () => {
-    const lastRulesItem = document.querySelector('.rules__item:last-child p')
-    const rulesLink = document.querySelector('.rules__link')
-    lastRulesItem.addEventListener('animationend', () => {
-      rulesLink.classList.add('rules__link--active')
-    })
+    const lastRulesItem = document.querySelector(`.rules__item:last-child p`);
+    const rulesLink = document.querySelector(`.rules__link`);
+    lastRulesItem.addEventListener(`animationend`, () => {
+      rulesLink.classList.add(`rules__link--active`);
+    });
   });
 };
